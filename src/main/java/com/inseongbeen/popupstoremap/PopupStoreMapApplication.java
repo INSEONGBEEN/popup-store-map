21a97277f6f7f864a46400d22089b636fd5d36fe
INSEONGBEEN <dlstjdqls0102@gmail.com>
2026-07-20 22:37:44 +0900

    Initial Spring Boot project

---
+++ src/main/java/com/inseongbeen/popupstoremap/PopupStoreMapApplication.java
@@ -0,0 +1,13 @@
+package com.inseongbeen.popupstoremap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopupStoreMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopupStoreMapApplication.class, args);
+	}
+
+}
